09a4bbd81a873e4b39f7191b64d3f8d67dcb930a
sahil <inglesahil98@gmail.com>
2025-01-20 17:20:48 +0530

    improved code

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Key Master",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
